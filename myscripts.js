9d5d67b358c1bf75a1b8c96f76dc7a5c8c1eb3d5
Bhaskar Jyoti Bhattacharya <shadowwarrior@pop-os.localdomain>
2022-03-19 20:58:41 +0530

    front-end done

---
+++ myscripts.js
@@ -0,0 +1,45 @@
+
+window.onload = hacking;//Llamamos a la funcion despues de que el documento ha sido cargado completamente
+function hacking(){
+	var c = document.getElementById("locoalien");
+	c.height = window.innerHeight;  //innerHeight se utiliza para saber la altura de la pantalla
+	c.width = window.innerWidth;    //innerHeight se utiliza para saber la altura de la pantalla
+
+	
+	var letraTam=15; //Tamaño de la letras por pixel
+	var columnas=c.width/letraTam; //El ancho dividido por el tamano que tendra las letras
+	
+	var letras=[];
+	for(var i=0; i<columnas;i++){
+		letras[i]=1;//Siver para saber la cantidad de letras que tendra en la pantalla
+	}
+	contexto= c.getContext('2d');//Muy importante especificar el contexto en el cual vamos a trabajar
+
+	function dibujar(){
+		contexto.fillStyle="rgba(0,0,0,0.05)";//Damos el color que tendra el recuadro en el que estara la animacion. en este caso sera transparente 0.05
+		contexto.fillRect(0,0,c.width,c.height);//Damos las dimensiones alto y ancho que tendra el cuadrado, que en este caso es de toda la pantalla
+
+		contexto.fillStyle= "#0f0";//Color de las letras
+		contexto.font= letraTam+"px arial";//Tamaño de la letra
+
+		for(var i=0;i<letras.length;i++){
+			text=elegirTexto();
+			Texto=text.split("");
+			contexto.fillText(Texto,i*letraTam, letras[i]*letraTam);//Para imprimir texto disponesmos de fillText(texto,x,y)
+			
+
+			if(letras[i]*letraTam > c.height && Math.random()>0.975){
+				letras[i]=0;
+			}
+			letras[i]++;
+
+		}
+
+	}
+		function elegirTexto(){
+		var numTexto=Math.floor((Math.random()*94)+33);//Elegimos un caso en base a un numero aleatorio
+				return String.fromCharCode(parseInt(numTexto));
+			}
+	setInterval(dibujar,120);
+
+}
